625e39bd0e782d33d4e38c13d5786151cfeed187
AdrianNextMD <adriano99pavaleanu@gmail.com>
2021-03-18 14:40:14 +0200

    Firts Commit

---
+++ Snake/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Obstacles : MonoBehaviour
+{
+    public bool magicSphere;
+    public bool Coin;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SnakeMain"))
+        {
+            if (magicSphere)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+
+        }
+    }
+}
